b90e72aeb0c14082fcc6dea06c04b5a640e6633e
Mistergrow <Misterfloppy@gmx.de>
2016-07-07 07:37:22 +0200

    Add files via upload

--- role.builder.js
+++ role.builder.js
@@ -22,8 +22,7 @@ var roleBuilder = {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
-                creep.moveTo(29,14);
-            }
+                }
         }
     }
 };
